3c153234fc476a0c4e80be7ece2190c1c3839c13
Alex Kamedov <alex@kamedov.ru>
2012-02-25 16:43:49 +0600

    refactor backend

--- django_cas/backends.py
+++ django_cas/backends.py
@@ -1,58 +1,70 @@
 """CAS authentication backend"""
 
+from django.conf import settings
+from django.contrib.auth.backends import ModelBackend
+from django.core.urlresolvers import get_callable
+from django_cas.models import User
 from urllib import urlencode, urlopen
 from urlparse import urljoin
 
-from django.conf import settings
-
-from django_cas.models import User
 
 __all__ = ['CASBackend']
 
-def _verify_cas1(ticket, service):
-    """Verifies CAS 1.0 authentication ticket.
 
-    Returns username on success and None on failure.
-    """
-
+def get_response(ticket, service):
+    """Make request to CAS and get response"""
     params = {'ticket': ticket, 'service': service}
-    url = (urljoin(settings.CAS_SERVER_URL, 'validate') + '?' +
+    if settings.CAS_VERSION == '1':
+        validate = 'validate'
+    else:
+        validate = 'serviceValidate'
+    url = (urljoin(settings.CAS_SERVER_URL, validate) + '?' +
            urlencode(params))
     page = urlopen(url)
+    response = None
     try:
-        verified = page.readline().strip()
-        if verified == 'yes':
-            return page.readline().strip()
-        else:
-            return None
+        response = page.read().strip()
     finally:
         page.close()
+    return response
 
 
-def _verify_cas2(ticket, service):
+def _verify_cas1(response):
+    """Verifies CAS 1.0 authentication ticket.
+
+    Returns user instance on success and None on failure.
+    """
+    if response is not None:
+        try:
+            verified, username = response.strip().split()
+            if verified == 'yes':
+                return get_user(username)
+        except:
+            pass
+    return None
+
+
+def _verify_cas2(response):
     """Verifies CAS 2.0+ XML-based authentication ticket.
 
-    Returns username on success and None on failure.
+    Returns user instance on success and None on failure.
     """
-
     try:
         from xml.etree import ElementTree
     except ImportError:
         from elementtree import ElementTree
 
-    params = {'ticket': ticket, 'service': service}
-    url = (urljoin(settings.CAS_SERVER_URL, 'proxyValidate') + '?' +
-           urlencode(params))
-    page = urlopen(url)
     try:
-        response = page.read()
         tree = ElementTree.fromstring(response)
         if tree[0].tag.endswith('authenticationSuccess'):
-            return tree[0][0].text
-        else:
-            return None
-    finally:
-        page.close()
+            user = get_user(tree[0][0].text)
+            if user is not None:
+                attr_filler = get_callable(settings.CAS_ATTRIBUTES_PROCESSOR)
+                if attr_filler:
+                    user = attr_filler(user, tree)
+                return user
+    except:
+        return None
 
 
 _PROTOCOLS = {'1': _verify_cas1, '2': _verify_cas2}
@@ -63,27 +75,20 @@ if settings.CAS_VERSION not in _PROTOCOLS:
 _verify = _PROTOCOLS[settings.CAS_VERSION]
 
 
-class CASBackend(object):
+def get_user(username):
+    """Returns user instance"""
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        # user will have an "unusable" password
+        user = User.objects.create_user(username, '')
+        user.save()
+    return user
+
+
+class CASBackend(ModelBackend):
     """CAS authentication backend"""
 
     def authenticate(self, ticket, service):
         """Verifies CAS ticket and gets or creates User object"""
-
-        username = _verify(ticket, service)
-        if not username:
-            return None
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            # user will have an "unusable" password
-            user = User.objects.create_user(username, '')
-            user.save()
-        return user
-
-    def get_user(self, user_id):
-        """Retrieve the user's entry in the User model if it exists"""
-
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+        return _verify(get_response(ticket, service))
